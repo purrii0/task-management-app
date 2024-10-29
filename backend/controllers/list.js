8f1e6075ddfe8507ab7f540a9a31eb75af26aa18
purrii0 <purrii0905@gmail.com>
2024-10-30 01:34:11 +0545

    crappy version of backend

--- backend/controllers/list.js
+++ backend/controllers/list.js
@@ -1,37 +1,86 @@
 const { User, Task, Todo } = require("../db/db");
-const auth = require("../auth/auth");
 
-const create = [auth, async (req, res) => {
+const create = async (req, res) => {
     try {
-        const userId = req.id;
+        const id = req.id;
         const { title } = req.body;
         await Todo.create({
-            userId,
+            owner: id,
             title
         })
         return res.status(200).json({ message: "Tdod successfully created" })
     } catch (error) {
         return res.status(400).json({ message: "Something Went Wrong" })
     }
-}];
+};
 
-const task = [auth, async (req, res) => {
+const task = async (req, res) => {
     try {
         const listid = req.params.listid;
-        const userId = req.id;
+        const id = req.id;
         const { title, priority, assignedBy } = req.body;
         await Task.create({
             title,
             priority,
             dueDate: new Date(),
             assignedBy,
-            assignedTo: userId
+            assignedTo: id
         })
         res.status(201).json({ message: "Task Succesfully Created" })
     } catch (error) {
         return res.status(400).json({ message: "Something Went Wrong" })
     }
-}]
+}
 
+const update = async (req, res) => {
+    try {
+        const listid = req.params.listid;
+        const taskid = req.params.taskid;
+        const id = req.id;
+        const { completed } = req.body;
+        await Task.findOneAndUpdate({
+            completed: completed
+        })
+        res.status(200).json({ message: "Task updated successfully" })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ message: "Something Went Wrong" })
+    }
+}
 
-module.exports = { create, task };
+const deleteTask = async (req, res) => {
+    try {
+        const listid = req.params.listid;
+        const taskid = req.params.taskid;
+        const id = req.id;
+        await Task.findOneAndDelete({ _id: taskid })
+        res.status(200).json({ message: "Task Deleted Successfully" })
+    } catch (error) {
+        return res.status(400).json({ message: "Something Went Wrong" })
+    }
+}
+
+const addCollabrator = async (req, res) => {
+    try {
+        const listid = req.params.listid;
+        const id = req.id;
+        const { userid } = req.body;
+        await Todo.findOneAndUpdate({ collabrators: userid })
+        res.status(200).json({ message: "Collaborator added successfully" })
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ message: "Something Went Wrong" })
+    }
+}
+
+const deleteCollabrator = async (req, res) => {
+    const listid = req.params.listid;
+    const userid = req.params.userid;
+    const todo = await Todo.findOne({ collabrators: userid });
+    let index = todo.collabrators.findIndex(user => user === userid)
+    todo.collabrators.splice(index, 1);
+    await todo.save();
+    res.status(200).json({ message: "Collabratore Removed" })
+}
+
+module.exports = { create, task, update, deleteTask, addCollabrator, deleteCollabrator };
--- backend/routes/list.js
+++ backend/routes/list.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const listRoute = express.Router();
-const { create, task } = require("../controllers/list")
+const { create, task, update, deleteTask, addCollabrator, deleteCollabrator } = require("../controllers/list")
+const auth = require("../auth/auth")
 
-listRoute.post("/create", create);
-listRoute.post("/:listid/task", create);
+listRoute.post("/create", auth, create);
+listRoute.post("/:listid/task", auth, task);
+listRoute.put("/:listid/task/:taskid", auth, update);
+listRoute.delete("/:listid/task/:taskid", auth, deleteTask);
+listRoute.post("/:listid/collabrator", auth, addCollabrator);
+listRoute.delete("/:listid/collabrator/:userid", auth, deleteCollabrator);
 
 module.exports = listRoute;
