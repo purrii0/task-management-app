d980d06e9d484d57e42bcc6496fab832c5dcc06d
purrii0 <purrii0905@gmail.com>
2024-10-29 21:14:30 +0545

    Added User Endpoint

---
+++ backend/db/db.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { Schema, ObjectId } = mongoose;
+
+const baseSchema = new Schema({
+    name: String,
+    username: String,
+    email: String,
+    password: String,
+    profilePicture: String,
+    notification: { type: Boolean, default: true },
+});
+
+const TaskSchema = new Schema({
+    title: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+    priority: {
+        type: String,
+        enum: ["low", "medium", "high"],
+        default: "medium",
+    },
+    dueDate: Date,
+    subtasks: [{ title: String, completed: { type: Boolean, default: false } }],
+    assignedBy: { type: ObjectId, ref: "users", required: true },
+    assignedTo: { type: ObjectId, ref: "users" },
+});
+
+const TodoSchema = new Schema({
+    owner: { type: ObjectId, ref: 'users' },
+    title: String,
+    tasks: [TaskSchema],
+    collabrators: [{ type: ObjectId, ref: 'users' }]
+})
+
+const User = mongoose.model("users", baseSchema)
+const Task = mongoose.model("tasks", TaskSchema)
+const Todo = mongoose.model("todos", TodoSchema)
+
+module.exports = {
+    User,
+    Task,
+    Todo
+}
